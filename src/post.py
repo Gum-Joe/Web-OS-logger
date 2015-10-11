8c1e2cffc1880ca67b380446f73e43627b6c6dff
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-10-11 12:22:49 +0100

    Fix posting + add packages

--- src/post.py
+++ src/post.py
@@ -2,7 +2,7 @@
 #Send post request
 import requests
 import sys
-pdata = str(sys.argv[1])
-r = requests.post(str(sys.argv[0]), data=pdata)
+pdata = str(sys.argv[2])
+r = requests.post(str(sys.argv[1]), data=pdata)
 print(r.status_code, r.reason)
 print(r.text[:300] + '...')
